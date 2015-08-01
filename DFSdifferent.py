e347e6bfd1cc4d59ef91c0d79e5fe970850c2651
vj-ug <vasujindal2015@gmail.com>
2015-08-01 10:47:39 -0500

    Another implementation of DFS

---
+++ DFSdifferent.py
@@ -0,0 +1,7 @@
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    for next in graph[start] - visited:
+        dfs(graph, next, visited)
+    return visited
